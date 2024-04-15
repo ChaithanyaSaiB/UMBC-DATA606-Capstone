eec853c4430eb31508acf5e711c2681332fe8749
ChaithanyaSaiB <125410517+ChaithanyaSaiB@users.noreply.github.com>
2024-04-15 18:28:42 -0400

    Cleaned App.py

--- app/app.py
+++ app/app.py
@@ -1,31 +1,3 @@
-
-"""from prediction import predict
-import streamlit as st
-from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-
-st.title('Classifying CNN Health News Articles from 2015')
-st.markdown('This is a topic modelling Natural Language Processsing model that would try to extract the topic of article from the URL passed in the text underneath')
-
-st.header("Give a news article link to know the topic it is related to")
-article_url = st.text_input('Article URL', '')
-
-if st.button("Find Topic"):
-  result, article_words = predict(article_url)
-  st.markdown("Topic seems to be most related to "+result)
-
-  st.markdown("Word Cloud for the article is as follows")
-  word_cloud = WordCloud(width = 800, height = 800,
-                background_color ='white',
-                min_font_size = 10).generate(' '.join(article_words))
-
-  fig = plt.figure(figsize = (8, 8), facecolor = None)
-  plt.imshow(word_cloud)
-  plt.axis("off")
-  plt.tight_layout(pad = 0)
-
-  st.pyplot(fig)"""
-
 from prediction import predict
 import streamlit as st
 from wordcloud import WordCloud
