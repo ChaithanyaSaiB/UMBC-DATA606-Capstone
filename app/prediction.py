056ff6395dbd0351d9af887f621cffa6298ed512
ChaithanyaSaiB <125410517+ChaithanyaSaiB@users.noreply.github.com>
2024-04-15 18:34:45 -0400

    Updated Prediction.py

--- app/prediction.py
+++ app/prediction.py
@@ -1,4 +1,4 @@
-import joblib
+"""import joblib
 import pandas as pd
 import re
 import requests
@@ -165,4 +165,156 @@ def predict(url):
   lda_model = load_model_from_github('https://raw.githubusercontent.com/ChaithanyaSaiB/UMBC-DATA606-Capstone/main/app/lda_model.sav')
   topics_probability = lda_model.get_document_topics(transformed_data)
   topic_number = max(topics_probability, key=lambda x: x[1])[0]
-  return index_to_topic.get(topic_number) , filtered_data
+  return index_to_topic.get(topic_number) , filtered_data"""
+
+
+import joblib
+import pandas as pd
+import re
+import requests
+from bs4 import BeautifulSoup
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('words')
+from nltk.corpus import wordnet
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from gensim.corpora.dictionary import Dictionary
+import streamlit as st
+import io
+
+# Function to fetch the content from a URL
+def fetch_content(url):
+    try:
+        response = requests.get(url, timeout=10)
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except Exception as e:
+        print("Error fetching content:", e)
+        return None
+
+# Function to extract body content from HTML using BeautifulSoup
+def extract_body(html_content):
+    if html_content:
+        soup = BeautifulSoup(html_content, 'html.parser')
+        article_body = soup.findAll('p', class_='paragraph')
+        if article_body:
+            return article_body
+        else:
+            return None
+    else:
+        return None
+
+# Function to split strings with special characters
+def split_string_with_special_characters(word_list):
+    pattern = r'[-\s]'
+    for word in word_list:
+        if re.search(r'[-\s]', word):
+            substrings = re.split(pattern, word)
+            substrings = [substr for substr in substrings if substr]
+            word_list.remove(word)
+            word_list.extend(substrings)
+    return word_list
+
+# Function to convert words to lowercase and lemmatize
+def lowercase_words_and_lemmatize(word_list):
+    word_list_lower = [word.lower() for word in word_list]
+    pos_tags = nltk.pos_tag(word_list_lower)
+
+    def get_wordnet_pos(tag):
+        if tag.startswith('J'):
+            return wordnet.ADJ
+        elif tag.startswith('V'):
+            return wordnet.VERB
+        elif tag.startswith('N'):
+            return wordnet.NOUN
+        elif tag.startswith('R'):
+            return wordnet.ADV
+        else:
+            return wordnet.NOUN
+
+    lemmatized_words = []
+    lemmatizer = WordNetLemmatizer()
+    for word, tag in pos_tags:
+        pos = get_wordnet_pos(tag)
+        lemma = lemmatizer.lemmatize(word, pos=pos)
+        lemmatized_words.append(lemma)
+
+    return lemmatized_words
+
+# Parts of speech to remove during stopwords removal
+parts_of_speech_to_remove = ['DT', 'IN', 'PRP', 'PRP$', 'CC', 'VB', 'JJ']
+
+# Function to remove stopwords
+def stopwords_removal(word_list):
+    tagged_words = nltk.pos_tag(word_list)
+    stop_words = set(stopwords.words('english'))
+
+    # Define the GitHub URL of the custom stopwords file
+    github_url = "https://github.com/ChaithanyaSaiB/UMBC-DATA606-Capstone/blob/main/app/custom%20stopwords.txt"
+    
+    # Read the custom stopwords from the GitHub URL
+    custom_stopwords = read_text_from_github(github_url).split()
+  
+    return [word for word, pos in tagged_words if pos not in parts_of_speech_to_remove 
+            and word not in stop_words and word not in custom_stopwords]
+
+# Function to remove non-alphabetic characters and custom stopwords
+def remove_non_alphabetic_and_custom_stopwords(word_list):
+    english_words = set(nltk.corpus.words.words())
+    return [word for word in [re.sub(r'[^a-zA-Z]', '', word) for word in word_list] if word in english_words]
+
+# Mapping topic index to topic name
+index_to_topic = {
+    0: "Vaccination and Measles",
+    1: "Medical Treatments and Marijuana",
+    2: "Health Research and Findings",
+    3: "Childhood Mental Health and Education",
+    4: "Infectious Diseases and Hygiene",
+    5: "Food and Nutrition",
+    6: "Family Health and Illness",
+    7: "Personal Health and Weight Management",
+    8: "Healthcare System and Cases"
+}
+
+# Function to read text from GitHub URL
+def read_text_from_github(url):
+    raw_url = url.replace("github.com", "raw.githubusercontent.com").replace("/blob/", "/")
+    response = requests.get(raw_url)
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
+
+# Function to load the model
+@st.cache(allow_output_mutation=True)
+def load_model_from_github(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        content = io.BytesIO(response.content)
+        model = joblib.load(content)
+        return model
+    else:
+        st.error(f"Failed to load model from {url}. Status code: {response.status_code}")
+
+# Function to predict topic of news article
+def predict(url):
+    raw_data = extract_body(fetch_content(url))
+    content = " ".join([p_tag.text.strip() for p_tag in raw_data])
+    tokenized_data = word_tokenize(content)
+    lemmatized_data = lowercase_words_and_lemmatize(tokenized_data)
+    filtered_data = remove_non_alphabetic_and_custom_stopwords(stopwords_removal(lemmatized_data))
+    
+    model_dictionary = Dictionary.load('https://raw.githubusercontent.com/ChaithanyaSaiB/UMBC-DATA606-Capstone/main/app/dictionary.sav')
+    transformed_data = model_dictionary.doc2bow(filtered_data)
+
+    lda_model = load_model_from_github('https://raw.githubusercontent.com/ChaithanyaSaiB/UMBC-DATA606-Capstone/main/app/lda_model.sav')
+    topics_probability = lda_model.get_document_topics(transformed_data)
+    topic_number = max(topics_probability, key=lambda x: x[1])[0]
+    return index_to_topic.get(topic_number) , filtered_data
