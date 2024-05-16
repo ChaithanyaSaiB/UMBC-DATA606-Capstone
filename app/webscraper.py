16b424548cf239e85b30fe75f36e0ca72bd3c247
ChaithanyaSaiB <125410517+ChaithanyaSaiB@users.noreply.github.com>
2024-05-16 14:52:39 -0400

    Update import in webscraper

--- app/webscraper.py
+++ app/webscraper.py
@@ -2,7 +2,7 @@ import requests
 from tqdm import tqdm
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse
-from fake_useragent import UserAgent
+from fake_useragent_main.src.fake_useragent import UserAgent
 '''
 # Function to fetch content from a URL
 def fetch_content(url):
